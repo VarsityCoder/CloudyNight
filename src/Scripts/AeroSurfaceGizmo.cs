145acb41563c2ea21d7c8bf11eead42ff2966428
kemba <miles.welsh@live.com>
2024-12-03 13:13:27 -0600

    Updating AeroSurfaceGizmo.cs

--- src/Scripts/AeroSurfaceGizmo.cs
+++ src/Scripts/AeroSurfaceGizmo.cs
@@ -1,7 +1,30 @@
 using Godot;
-using System;
+namespace CloudyNight.Scripts;
+public partial class AeroSurfaceGizmo : EditorNode3DGizmoPlugin {
+  private static readonly float _wingOpacity = 0.2f;
+  private StandardMaterial3D _wingMaterial = new();
+  private Color _wingColor = new Color(1,1,1, _wingOpacity);
+  private Color _flapColor = new Color(1, 1, 0, _wingOpacity);
 
-public partial class AeroSurfaceGizmo : EditorNode3DGizmoPlugin
-{
+  private void Init() {
+    _wingMaterial.SetShadingMode(BaseMaterial3D.ShadingModeEnum.Unshaded);
+    _wingMaterial.SetTransparency(BaseMaterial3D.TransparencyEnum.Disabled);
+    _wingMaterial.SetCullMode(BaseMaterial3D.CullModeEnum.Disabled);
+    _wingMaterial.VertexColorUseAsAlbedo = true;
+    _wingMaterial.SetFlag(BaseMaterial3D.Flags.DisableDepthTest, true);
+  }
 
+  private string GetGizmoName() => "AeroSurfaceGizmo";
+
+  private bool HasGizmo(Node3D forNode3D) {
+    //TODO need to change this to AeroSurface3d
+    return forNode3D is AeroBody3d;
+  }
+  private void Redraw(EditorNode3DGizmo gizmo) {
+    gizmo.Clear();
+    var spatial = gizmo.GetNode3D();
+    var st = new SurfaceTool();
+
+    var halfChord = (float) spatial.Get("wing_config.chord") / 2f;
+  }
 }
