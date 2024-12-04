b307b0ec9f95a0b882e304fd3a8e31d580d4a6ea
kemba <miles.welsh@live.com>
2024-12-03 20:54:53 -0600

    Updating AeroSurfaceGizmo.cs

--- src/Scripts/AeroSurfaceGizmo.cs
+++ src/Scripts/AeroSurfaceGizmo.cs
@@ -25,6 +25,48 @@ public partial class AeroSurfaceGizmo : EditorNode3DGizmoPlugin {
     var spatial = gizmo.GetNode3D();
     var st = new SurfaceTool();
 
+    // origin
     var halfChord = (float) spatial.Get("wing_config.chord") / 2f;
+    var quarterChord = (float)spatial.Get("wing_config.chord") / 4f;
+    var halfSpan = (float)spatial.Get("wing_config.span") / 2f;
+
+    st.Begin(Mesh.PrimitiveType.Triangles);
+    // flap section
+    var tl = new Vector3(-halfSpan, 0, halfChord);
+    var tr = new Vector3(halfSpan, 0, halfChord);
+    tl.Z += quarterChord;
+    tr.Z += quarterChord;
+    var bl = new Vector3(-halfSpan, 0, quarterChord);
+    var br = new Vector3(halfSpan, 0, quarterChord);
+
+    //first triangle
+    st.SetColor(_flapColor);
+    st.AddVertex(tl);
+    st.AddVertex(tr);
+    st.AddVertex(bl);
+    //second triangle
+    st.AddVertex(bl);
+    st.AddVertex(tr);
+    st.AddVertex(br);
+
+    //wing section
+    var tlwing = new Vector3(-halfSpan, 0, quarterChord);
+    var trwing = new Vector3(halfSpan, 0, quarterChord);
+    var blwing = new Vector3(-halfSpan, 0, -halfChord + quarterChord);
+    var brwing = new Vector3(halfSpan, 0, -halfChord + quarterChord);
+
+    //first triangle
+    st.SetColor(_wingColor);
+    st.AddVertex(tlwing);
+    st.AddVertex(trwing);
+    st.AddVertex(blwing);
+    //second triangle
+    st.AddVertex(blwing);
+    st.AddVertex(trwing);
+    st.AddVertex(brwing);
+
+    var mesh = st.Commit();
+    gizmo.AddMesh(mesh, _wingMaterial);
+    gizmo.AddCollisionTriangles(mesh.GenerateTriangleMesh());
   }
 }
