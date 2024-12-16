694f1b40dd8f1020ac6c2a70ba7cfefca027059a
kemba <miles.welsh@live.com>
2024-12-16 09:24:47 -0600

    Adding DungeonRoom3D.cs

---
+++ src/SimpleDungeons/DungeonRoom3D.cs
@@ -0,0 +1,14 @@
+namespace CloudyNight.SimpleDungeons;
+
+using Godot;
+
+
+public partial class DungeonRoom3D : Node3D {
+  public DungeonRoom3D VirtualizedFrom;
+
+
+
+  public void AddDebugViewIfNotExist() {
+
+  }
+}
