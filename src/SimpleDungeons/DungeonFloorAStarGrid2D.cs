5360a2ca6706e8269760244624f0294b91077fdf
kemba <miles.welsh@live.com>
2024-12-19 13:22:45 -0600

    Updating DungeonFloorAStarGrid2D.cs

--- src/SimpleDungeons/DungeonFloorAStarGrid2D.cs
+++ src/SimpleDungeons/DungeonFloorAStarGrid2D.cs
@@ -1,7 +1,20 @@
 namespace CloudyNight.SimpleDungeons;
+
+using System.Linq;
 using Godot;
+using Godot.Collections;
 
+public partial class DungeonFloorAStarGrid2D : AStarGrid2D {
+  private Array<Vector2I> _corridors = [];
 
-public partial class DungeonFloorAStarGrid2D : AStarGrid2D
-{
+  private int ComputeCost(Vector2I from, Vector2I to) {
+    if (_corridors.Contains(to)) {
+      return 0;
+    }
+    return (int)CellSize.X ;
+  }
+
+  private int EstimateCost() => 0;
+
+  private void AddCorridor(Vector2I xz) => _corridors = (Array<Vector2I>)_corridors.Append(xz);
 }
